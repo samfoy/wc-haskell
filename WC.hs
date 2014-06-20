d90755e4d2726d16c8c758039f031329a4c9662a
Samuel Painter <sam.foy.painter@gmail.com>
2014-06-20 18:09:43 -0400

    updated gitignore

--- WC.hs
+++ WC.hs
@@ -1,11 +1,13 @@
-import System.Environment
-import qualified Data.Map as Map
+module Main(main) where
 
-options :: Map.Map Char (String -> Int)
-options = Map.fromList[
-	('c',charCount),
-	('l',lineCount),
-	('w',wordCount)]
+import System.Environment
+--import qualified Data.Map as Map
+
+--options :: Map.Map Char (String -> Int)
+--options = Map.fromList[
+--	('c',charCount),
+--	('l',lineCount),
+--	('w',wordCount)]
 
 main :: IO ()
 main = do
@@ -14,21 +16,18 @@ main = do
 		then interact $ compose [charCount, lineCount, wordCount]
 	else
 		case head args of
-			"-c" -> interact $ anyCount charCount
-			"-l" -> interact $ anyCount lineCount
-			"-w" -> interact $ anyCount wordCount
+			"-c" -> interact $ compose [charCount]
+			"-l" -> interact $ compose [lineCount]
+			"-w" -> interact $ compose [wordCount]
 			_ -> usage
 
 usage :: IO ()
-usage = putStrLn "Usage: WC [-clw]"
+usage = putStrLn "Usage: wc [-clw] [file,...]"
 
 compose :: [String -> Int] -> String -> String
 compose [] xs = show (wordCount xs) ++ "\n"
 compose args xs = concatMap (\f -> show f ++ "\t") os ++ "\n"
 	where os = map ($ xs) args
-
-anyCount :: (String -> Int) -> String -> String
-anyCount f s = show (f s) ++ "\n"
 
 charCount :: String -> Int
 charCount = length
